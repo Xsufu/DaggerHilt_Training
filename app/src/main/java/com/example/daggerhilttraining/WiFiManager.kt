e6a9e16f612ef7f8540b722cb960f59069cd53a3
Xsufu <aluno.macarrao@gmail.com>
2023-07-16 19:21:55 +0700

    Добавлены комментарии по работе аннотаций для класса

--- app/src/main/java/com/example/daggerhilttraining/WiFiManager.kt
+++ app/src/main/java/com/example/daggerhilttraining/WiFiManager.kt
@@ -1,11 +1,18 @@
 package com.example.daggerhilttraining
 
 import android.util.Log
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
 import javax.inject.Singleton
 
-// Класс WiFiManager зависит от WiFiSettings
+/* Класс WiFiManager зависит от WiFiSettings */
+/* С аннотацией Singleton доступ к одной инстанции класса осуществляется во всём приложении */
+
+/* ActivityRetainedScoped даёт доступ к одной инстанции
+   при изменении конфигурации и возможность поделиться с ViewModel в пределах одной Activity */
+
 @Singleton
+//@ActivityRetainedScoped
 class WiFiManager @Inject constructor(private val settings: WiFiSettings) {
     fun connect() {
         settings.openConnection()
