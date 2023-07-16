57ebe8216ac8aaba984da885a9b4a3da62a3cf4a
Xsufu <aluno.macarrao@gmail.com>
2023-07-16 18:14:01 +0700

    Добавлен пример инициализации классов без Hilt

--- app/src/main/java/com/example/daggerhilttraining/MainActivity.kt
+++ app/src/main/java/com/example/daggerhilttraining/MainActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var wiFiManager: WiFiManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settings = WiFiSettings()
+
+        wiFiManager = WiFiManager(settings)
+
+        wiFiManager.connect()
+        wiFiManager.sendMessage()
     }
 }
