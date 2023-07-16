504694a1f631d3f0938fa0e366136f1b905c64fb
Xsufu <aluno.macarrao@gmail.com>
2023-07-16 19:15:27 +0700

    Добавлена Singleton аннотация к WiFiManager классу

--- app/src/main/java/com/example/daggerhilttraining/WiFiManager.kt
+++ app/src/main/java/com/example/daggerhilttraining/WiFiManager.kt
@@ -2,8 +2,10 @@ package com.example.daggerhilttraining
 
 import android.util.Log
 import javax.inject.Inject
+import javax.inject.Singleton
 
 // Класс WiFiManager зависит от WiFiSettings
+@Singleton
 class WiFiManager @Inject constructor(private val settings: WiFiSettings) {
     fun connect() {
         settings.openConnection()
