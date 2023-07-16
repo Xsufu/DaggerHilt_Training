aac3174fbf6b6471d809bf175f49bd3b19970d4e
Xsufu <aluno.macarrao@gmail.com>
2023-07-16 18:33:11 +0700

    Тренировочные классы проинициализированы при помощи Hilt

--- app/src/main/java/com/example/daggerhilttraining/MainActivity.kt
+++ app/src/main/java/com/example/daggerhilttraining/MainActivity.kt
@@ -2,17 +2,17 @@ package com.example.daggerhilttraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var wiFiManager: WiFiManager
+    @Inject
+    lateinit var wiFiManager: WiFiManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val settings = WiFiSettings()
-
-        wiFiManager = WiFiManager(settings)
 
         wiFiManager.connect()
         wiFiManager.sendMessage()
